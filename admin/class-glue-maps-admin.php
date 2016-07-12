39d5e3149ce2649dee9a2060cd67c970d27dbea2
Mathenarian <hencb@openmailbox.org>
2016-07-12 12:11:07 +0200

    Fixed some formating issues

--- admin/class-glue-maps-admin.php
+++ admin/class-glue-maps-admin.php
@@ -59,19 +59,6 @@ class Glue_Maps_Admin {
 	 *
 	 * @since    1.0.0
 	 */
-	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Glue_Maps_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Glue_Maps_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
 
 	public function add_plugin_admin_menu() {
 
@@ -93,10 +80,25 @@ class Glue_Maps_Admin {
 
 	}
 
+	public function enqueue_styles() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Glue_Maps_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Glue_Maps_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 
 		'css/glue-maps-admin.css', array(), $this->version, 'all' );
-
 	}
+
+
+	
 
 	/**
 	 * Register the JavaScript for the admin area.
