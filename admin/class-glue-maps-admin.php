96048671b6559df80c47b8d0d4df5ecec6112c73
Mathenarian <hencb@openmailbox.org>
2016-07-12 12:21:22 +0200

    Fix 2

--- admin/class-glue-maps-admin.php
+++ admin/class-glue-maps-admin.php
@@ -66,11 +66,13 @@ class Glue_Maps_Admin {
 	}
 
 	public function add_action_links( $links ) {
-
-	$settings_link = array(
-	 '<a href="' . admin_url('options-general.php?page=' . #this->plugin_name ) . '">' . __('Settings', $this->plugin_name) . '</a>');
-	return array_merge( $settings_link, $links);
-
+    /*
+    *  Documentation : https://codex.wordpress.org/Plugin_API/Filter_Reference/plugin_action_links_(plugin_file_name)
+    */
+   $settings_link = array(
+    '<a href="' . admin_url( 'options-general.php?page=' . $this->plugin_name ) . '">' . __('Settings', $this->plugin_name) . '</a>',
+   );
+   return array_merge(  $settings_link, $links );
 
 	}
 
